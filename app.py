bf2fde4ce797e1b21ee4a90ad82a8506bd0e9829
John Bumgardner <john.bumgardner@icloud.com>
2022-03-01 11:37:49 -0500

    add upload to s3 bucket and compute score for 2 players

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 import boto3
 
-def detect_text(photo, bucket):
+bucket='photos-for-golf-scoring'
 
+def detect_text(photo):
+    s3 = boto3.resource('s3')
     client=boto3.client('rekognition')
 
+    s3.meta.client.upload_file(photo, bucket, photo)
     response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
                         
-    textDetections=response['TextDetections']
-    print ('Detected text\n----------')
-    for text in textDetections:
-            print ('Detected text:' + text['DetectedText'])
-    return len(textDetections)
+    text_detections=response['TextDetections']
+    all_detected_chars = []    
+    for entry in text_detections:
+        text = entry['DetectedText']
+        for ch in text:
+            if not ch == ' ':
+                all_detected_chars.append(ch)
+        
+    return all_detected_chars[0:18]
+
+def sum_player(scores):
+    sum = 0
+    for i in scores:
+        sum += int(i)
+    return sum
 
 def main():
-
-    bucket='photos-for-golf-scoring'
     photo='score_card.jpg'
-    text_count=detect_text(photo,bucket)
-    print("Text detected: " + str(text_count))
-
+    text_count = detect_text(photo)
+    player1 = text_count[0:9]
+    player2 = text_count[10:18]
+    sum1 = sum_player(player1)
+    sum2 = sum_player(player2)
+    print(sum1)
+    print(sum2)
 
 if __name__ == "__main__":
     main()
